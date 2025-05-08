54d8bf1f47f100c2f8bde78891f6e1be1638450a
Amandev25 <amanyadav.dev25@gmail.com>
2025-05-08 22:55:43 +0530

    Advanced Routing

--- 1_Routing/index.js
+++ 1_Routing/index.js
@@ -2,15 +2,34 @@ import express from "express";
 const app = express();
 
 // Basic Routing..
-app.get("/" , (req,res)=>{
-    res.send("Home Page");
-});
-app.get("/about" , (req,res)=>{
-    res.send("<h1>Welcome to the about page!❤️❤️</h1>");
-});
-app.get("/Contacts" , (req,res)=>{
-    res.send("<h1>Welcome to the contact Page! </h1>");
-});
+// app.get("/" , (req,res)=>{
+//     res.send("Home Page");
+// });
+// app.get("/about" , (req,res)=>{
+//     res.send("<h1>Welcome to the about page!❤️❤️</h1>");
+// });
+// app.get("/Contacts" , (req,res)=>{
+//     res.send("<h1>Welcome to the contact Page! </h1>");
+// });
 
-app.listen(8000 , ()=> console.log("Server is up")
-);
+// app.listen(8000 , ()=> console.log("Server is up")
+// );
+
+// *****************Advnced Routing *********************
+    // HTTP Methods
+    // GET👉 Retrive data
+    // POST👉 Create/insert Data
+    // PUT👉 Completely update data
+    // PATCH👉 Partially Update Data
+    // DELETE👉 Delete Data
+    // ALL👉 Any HTTP Request Method (We use this very Rarely)
+
+   // STRING PATTERSNS
+   // regex 
+
+   // NEsted Routing
+    app.get("/products/iphone" , (req , res)=>{
+        res.send("<h1>This code will run if you provide /product/iphone</h1>");
+    })
+
+    app.listen(8000 , ()=> console.log("Server is up"));
