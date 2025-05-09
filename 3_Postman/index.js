42371f74c94706fcef9d1533376f53155981d02b
Amandev25 <amanyadav.dev25@gmail.com>
2025-05-09 09:13:27 +0530

    code refactor using app.route

--- 3_Postman/index.js
+++ 3_Postman/index.js
@@ -8,18 +8,28 @@
 import express from 'express';
 const app =express();
 // Ugly code
-app.get('/students', (req,res)=>{
-    res.send('All Students');
-})
-app.post('/students', (req,res)=>{
-    res.send('Add new Students');
-})
-app.put('/students', (req,res)=>{
-    res.send('update Students');
-})
+// app.get('/students', (req,res)=>{
+//     res.send('All Students');
+// })
+// app.post('/students', (req,res)=>{
+//     res.send('Add new Students');
+// })
+// app.put('/students', (req,res)=>{
+//     res.send('update Students');
+// })
 
-app.delete('/students', (req,res)=>{
-    res.send('Delete Students');
-})
+// app.delete('/students', (req,res)=>{
+//     res.send('Delete Students');
+// })
+
+// Refactor code
+// **************** app.route() ********************
+   
+ app
+ .route('/student')
+ .get((req,res)=>{res.send("ALL Students")})
+ .post((req,res)=>{res.send("Add new students")})
+ .put((req,res)=> {res.send("update stuudents")})
+ .delete((req,res)=>res.send("Delete students"));
 
 app.listen(8000,() =>console.log('Server is up'));
