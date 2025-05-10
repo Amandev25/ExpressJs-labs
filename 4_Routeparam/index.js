c6cf6625e3bd20c9197b116707cc5837d8b17f39
Amandev25 <amanyadav.dev25@gmail.com>
2025-05-10 07:22:08 +0530

    Third commit

--- 4_Routeparam/index.js
+++ 4_Routeparam/index.js
@@ -2,12 +2,22 @@ import express from 'express';
 const app = express();
 // Route parameters
 // Ecom/products/iphone/:iphoneId
-app.get('/product/order/:day/:month/:year' , (req,res)=>{
-   // res.send("User deleted");
-   // res.send(req.params.model);
-   const {day,month,year} =req.params;
-   res.send(`This product was ordered on ${day}/${month}/${year}`);
-})
+// app.get('/product/order/:day/:month/:year' , (req,res)=>{
+//    // res.send("User deleted");
+//    // res.send(req.params.model);
+//    const {day,month,year} =req.params;
+//    res.send(`This product was ordered on ${day}/${month}/${year}`);
+// })
 
+// app.param() : used to define middleware for route parameters
+console.clear();
+app.param('id' , (req,res)=>{
+    console.log(`Id : ${id}`);
+    next();
+});
+app.get("/user/:id" ,(req,res)=>{
+    console.log("This is User ID Path");
+    res.send("Response Ok!!");
+})
 
 app.listen(8000 , ()=> console.log("server up!!"))
