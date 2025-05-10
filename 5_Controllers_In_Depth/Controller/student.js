377cd6341d2d89f40485c114dcb25528aee1bfd6
Amandev25 <amanyadav.dev25@gmail.com>
2025-05-10 09:01:53 +0530

    Commit 1

--- 5_Controllers_In_Depth/Controller/student.js
+++ 5_Controllers_In_Depth/Controller/student.js
@@ -1,9 +1,9 @@
 import express from 'express';
 //const router =express.Router();
 
-const AllStudents =(req,res) => res.send('All Students');
-const createStudent = (req,res) => res.send('All new user');
-const updateStudent = (req,res) => res.send('Update user');
-const deleteStudent = (req,res) => res.send('Delete User');
+const AllStudents =(req,res) => {res.send('All Students');}
+const createStudent = (req,res) => {res.send('All new user');}
+const updateStudent = (req,res) => {res.send('Update user');}
+const deleteStudent = (req,res) => {res.send('Delete User');}
 
 export {AllStudents, createStudent, updateStudent, deleteStudent};
