ca97ed067e8b499771846dcf928a66537d44e777
Amandev25 <amanyadav.dev25@gmail.com>
2025-05-14 12:32:02 +0530

    Minor changes

---
+++ 9_ServingStatic/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import path from 'path';
+const app = express();
+//.static is a built-in middleware function in Express. It serves static files and is based on serve-static.
+// It is used to serve static files such as images, CSS files, and JavaScript files.
+app.use(express.static("./public"))
+
+app.get("/" ,(req,res)=>{
+    res.sendFile();
+})
+
+
+app.listen(8000 , ()=> console.log("Server is up"));
