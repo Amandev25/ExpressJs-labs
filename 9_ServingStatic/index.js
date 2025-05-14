786d13f365a44df02872e029aeadbdf8079c5d29
Amandev25 <amanyadav.dev25@gmail.com>
2025-05-14 13:04:56 +0530

    Index.js commit

--- 9_ServingStatic/index.js
+++ 9_ServingStatic/index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static("./public"))
 
 app.get("/" ,(req,res)=>{
-    res.sendFile();
+    res.sendFile(path.join(process.cwd(), "./public/index.html"));
 })
 
 
