61616a0d2aca4f912ab9c9dbacfd020f35e56558
Amandev25 <amanyadav.dev25@gmail.com>
2025-05-14 15:22:27 +0530

    Route added

--- 10_TempEngine/Routes/route.js
+++ 10_TempEngine/Routes/route.js
@@ -1,5 +1,9 @@
 import express from 'express';
 const router = express.Router();
+import {homecontroller} from '../Controllers/Home.js'
+import {aboutcontroller} from '../Controllers/About.js'
 
-router.get('/' ,)
-router.get('/about' ,)
+router.get('/' ,homecontroller)
+router.get('/about' , aboutcontroller)
+
+export default router;
