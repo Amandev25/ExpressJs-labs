a8f818820ace9485bbc2200670d643fcfadf232e
Amandev25 <amanyadav.dev25@gmail.com>
2025-05-15 08:34:38 +0530

    index.js file added

--- 10_TempEngine/index.js
+++ 10_TempEngine/index.js
@@ -1,12 +1,11 @@
 import express from 'express';
 import path from 'path';
 import router from './Routes/route.js';
-
 const app = express();
 
-app.set('View Engine' , "ejs");
+app.set('view engine',"ejs");
 
-app.use("/" ,router);
+app.use("/", router);
 
 app.listen(8000 ,() => console.log('Server is running on 8000')
 )
