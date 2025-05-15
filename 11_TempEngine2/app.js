d69a67399bb8eac3ff0994ab6eda896e31b97ad9
Amandev25 <amanyadav.dev25@gmail.com>
2025-05-15 08:28:17 +0530

    Bog Solved

--- 11_TempEngine2/app.js
+++ 11_TempEngine2/app.js
@@ -1,10 +1,13 @@
 import express from 'express';
 const app = express();
 
-app.set('view engine' , 'ejs');
+app.set('view engine', 'ejs');
 
 app.get('/' , (req, res) => {
-    res.render('index');
+    res.render('index',{
+        user: "Aman",
+        title : "EJS Template Engine"
+    });
 })
 
 app.listen(8000 , () => console.log('Server is Running on Port 8000')
