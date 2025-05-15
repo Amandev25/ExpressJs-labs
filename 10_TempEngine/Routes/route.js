28fcf8509d995d432e08a41bfebc4d953726fe34
Amandev25 <amanyadav.dev25@gmail.com>
2025-05-15 08:33:59 +0530

    Routes added

--- 10_TempEngine/Routes/route.js
+++ 10_TempEngine/Routes/route.js
@@ -1,9 +1,9 @@
 import express from 'express';
 const router = express.Router();
-import {homeController} from '../Controllers/Home.js'
-import {aboutController} from '../Controllers/About.js'
+import {homeController} from '../Controllers/Home.js' ;
+import {aboutController} from '../Controllers/About.js' ;
 
-router.get('/' ,homeController)
-router.get('/about' , aboutController)
+router.get('/',homeController);
+router.get('/about',aboutController);
 
 export default router;
