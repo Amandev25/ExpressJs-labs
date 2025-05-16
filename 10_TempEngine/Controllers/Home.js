cc6acb4463d6a20933140b783856583e0980e098
Amandev25 <amanyadav.dev25@gmail.com>
2025-05-16 11:50:17 +0530

    piyush garg

--- 10_TempEngine/Controllers/Home.js
+++ 10_TempEngine/Controllers/Home.js
@@ -1,5 +1,10 @@
 const homeController  = (req,res) => {
-res.render("home");
+    const data = {
+    name: "Aman Yadav",
+    age : 22,
+    userId : 20
+    }
+res.render("home" , data);
 };
 
 export {homeController}; 
--- 9_ServingStatic/index.js
+++ 9_ServingStatic/index.js
@@ -3,11 +3,11 @@ import path from 'path';
 const app = express();
 //.static is a built-in middleware function in Express. It serves static files and is based on serve-static.
 // It is used to serve static files such as images, CSS files, and JavaScript files.
-app.use(express.static("./public"))
+// app.use(express.static("./public"))
 
-app.get("/" ,(req,res)=>{
-    res.sendFile(path.join(process.cwd(), "./public/index.html"));
-})
+// app.get("/" ,(req,res)=>{
+//     res.sendFile(path.join(process.cwd(), "./public/index.html"));
+// })
 
 
 app.listen(8000 , ()=> console.log("Server is up"));
