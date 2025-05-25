670e64e6e3ca3dbbaa227783ae35e8d230e5ba2a
Amandev25 <amanyadav.dev25@gmail.com>
2025-05-25 23:52:32 +0530

    RestApi 2

--- Piyush_garg/Set_2/index.js
+++ Piyush_garg/Set_2/index.js
@@ -1,9 +1,11 @@
 import express from "express";
+import fs from "fs";
 const app = express();
 const port = 8000;
 import users from "./MOCK_DATA.json" assert { type: "json" };
 
-
+// Middleware
+app.use(express.urlencoded({ extended : false}));
 //Routes
 app.get("/api/users", (req,res) => {
     return res.json(users)
@@ -13,7 +15,16 @@ app.get("/api/users/:id", (req,res) => {
     const user = users.find((user)=> user.id === id);
     return res.json(user);
 });
-
+app.post("/api/users" , (req,res)=> {
+   const body = req.body;
+   users.push({...body , id: users.length + 1});
+   fs.writeFile("./MOCK_DATA.json", JSON.stringify(users),(err ,data) => {
+   return res.json({status: "pending"});
+   })
+   
+   
+   
+});
 
 
 
