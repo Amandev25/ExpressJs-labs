d8dee4eb6351f2511b1106d2e9a33df7b1f8e9a6
Amandev25 <amanyadav.dev25@gmail.com>
2025-05-27 00:34:55 +0530

    middleware

---
+++ Piyush_garg/Set_3/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+const app = express();
+const port = 8000;
+import users from "./MOCK_DATA.json" assert { type: "json" };
+
+app.use(express.urlencoded({ extended : false}));
+app.use((req,res,next)=>{
+    console.log("Hello from Middleware 1");
+   next();
+})
+app.use((req,res,next)=>{
+    console.log("Hello from Middleware 2");
+   return res.end("Ended response")
+})
+
+app.get("/users", (req,res) => {
+    return res.json(users)
+});
+
+
+
+app.listen(port , () => console.log(`Server is Running on port ${port}`));
