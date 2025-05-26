621bb04aa7a53f141a9e6b4498142897177c8c43
Amandev25 <amanyadav.dev25@gmail.com>
2025-05-27 00:33:26 +0530

    Status

--- Piyush_garg/Set_2/index.js
+++ Piyush_garg/Set_2/index.js
@@ -8,15 +8,21 @@ import users from "./MOCK_DATA.json" assert { type: "json" };
 app.use(express.urlencoded({ extended : false}));
 //Routes
 app.get("/api/users", (req,res) => {
+    res.setHeader("X-MYHEADER" , "Aman~Yadav")
     return res.json(users)
 });
 app.get("/api/users/:id", (req,res) => {
     const id = Number(req.params.id);
     const user = users.find((user)=> user.id === id);
+    if(!user){
+        return res.status(404).json({error : "User not Found"})
+    }
     return res.json(user);
 });
 app.post("/api/users" , (req,res)=> {
    const body = req.body;
+   if(!body.first_name || !body.last_name || !body.email || !!body.gender || !body.Job- Title || !body.id){
+    return res.status(400).json({error : "Please provide all required details"})}
    users.push({...body , id: users.length + 1});
    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users),(err ,data) => {
    return res.json({status: "pending"});
